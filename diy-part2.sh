408c4c9f94a02116720edcada61ac123694df4a8
QuitFlounder <quitflounder@gmail.com>
2026-03-08 17:11:20 +0800

    adding l2tp

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,3 +9,14 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+
+cat >> .config <<'EOF'
+CONFIG_PACKAGE_kmod-l2tp=y
+CONFIG_PACKAGE_kmod-pppol2tp=y
+CONFIG_PACKAGE_ppp-mod-pppol2tp=y
+CONFIG_PACKAGE_xl2tpd=y
+EOF
+
+make defconfig
+echo "11111111111111111111111111111111111111111111111"
+grep -E 'CONFIG_PACKAGE_(kmod-l2tp|kmod-pppol2tp|ppp-mod-pppol2tp|xl2tpd)=' .config || true
