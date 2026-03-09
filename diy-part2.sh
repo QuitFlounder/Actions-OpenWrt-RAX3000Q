f0eaf5b4533166105ee149874398b0d93ac1a05a
QuitFlounder <quitflounder@gmail.com>
2026-03-09 15:54:20 +0800

    add wireguard

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,8 +15,13 @@ CONFIG_PACKAGE_kmod-l2tp=y
 CONFIG_PACKAGE_kmod-pppol2tp=y
 CONFIG_PACKAGE_ppp-mod-pppol2tp=y
 CONFIG_PACKAGE_xl2tpd=y
+CONFIG_PACKAGE_kmod-wireguard=y
+CONFIG_PACKAGE_luci-app-wireguard=y
+CONFIG_PACKAGE_luci-proto-wireguard=y
+CONFIG_PACKAGE_wireguard-tools=y
 EOF
 
 make defconfig
 echo "11111111111111111111111111111111111111111111111"
 grep -E 'CONFIG_PACKAGE_(kmod-l2tp|kmod-pppol2tp|ppp-mod-pppol2tp|xl2tpd)=' .config || true
+grep -E 'CONFIG_PACKAGE_(kmod-wireguard|luci-app-wireguard|luci-proto-wireguard|wireguard-tools)=' .config || true
